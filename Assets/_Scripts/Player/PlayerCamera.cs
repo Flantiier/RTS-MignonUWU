06f97084b7908f24e0d0b8d74860c85777ef52bb
Florian Lernould <florianlernould7@gmail.com>
2023-04-18 15:55:41 +0200

    Camera bounds

--- Assets/_Scripts/Player/PlayerCamera.cs
+++ Assets/_Scripts/Player/PlayerCamera.cs
@@ -7,6 +7,8 @@ public class PlayerCamera : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private float speed = 3f;
     [SerializeField, Range(0, 0.1f)] private float lerp = 0.05f;
+    [SerializeField] private Vector2 boundsX;
+    [SerializeField] private Vector2 boundsZ;
     private Vector2 _rawInputs;
     private Vector2 _currentInputs;
     private Vector2 _SmoothInputRef;
@@ -50,8 +52,11 @@ public class PlayerCamera : MonoBehaviour
     {
         _currentInputs = Vector2.SmoothDamp(_currentInputs, _rawInputs, ref _SmoothInputRef, lerp);
         Vector3 movement = Vector3.forward * _currentInputs.y + Vector3.right * _currentInputs.x;
+        Vector3 position = transform.position + speed * Time.deltaTime * movement;
+        position.x = Mathf.Clamp(position.x, boundsX.x, boundsX.y);
+        position.z = Mathf.Clamp(position.z, boundsZ.x, boundsZ.y);
 
-        transform.position += speed * Time.deltaTime * movement;
+        transform.position = position;
     }
     #endregion
 }
